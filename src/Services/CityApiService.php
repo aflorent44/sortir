c536c15de9f9f04cbc070fde94e914a09042492d
VioletteCrochet <violettecrc@gmail.com>
2025-02-24 16:47:07 +0100

    feat(Cities) : add cityApi connexion

---
+++ src/Services/CityApiService.php
@@ -0,0 +1,28 @@
+<?php
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class CityApiService
+{
+    public function __construct(private HttpClientInterface $client,) {}
+
+    public function fetchGitHubInformation(): array
+    {
+        $apiUrl = $_ENV['CITY_API_URL'];
+        $response = $this->client->request(
+            'GET',
+            $apiUrl
+        );
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        $content = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+
+        return $content;
+    }
+}
