8b10902ec0cbd6affff9037f932e8f64960af148
VioletteCrochet <violettecrc@gmail.com>
2025-02-26 16:44:08 +0100

    feat(admin, users) : wip deletion of user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,7 +10,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/user', name: 'user_')]
@@ -31,6 +33,7 @@ final class UserController extends AbstractController
     #[Route('/update/{id}', name:'update', requirements: ['id'=>'\d+'])]
     public function updateProfil(User $user, Request $request, EntityManagerInterface $em): Response
     {
+
         $profilForm = $this->createForm(RegistrationFormType::class, $user);
         $profilForm->handleRequest($request);
 
@@ -42,7 +45,28 @@ final class UserController extends AbstractController
         return $this->render('user\update.html.twig', [
             'controller_name' => 'Modifier mes infos : ',
             'profilForm' => $profilForm,
+            'user' => $user,
         ]);
     }
-    
+
+    #[Route('/delete/{id}', name:'delete', requirements: ['id'=>'\d+'], methods: ['POST'])]
+    public function deleteProfil(User $user, EntityManagerInterface $em, TokenStorageInterface $tokenStorage, SessionInterface $session): Response
+    {
+        $user=$em->getRepository(User::class)->find($user->getId());
+        $em->remove($user);
+        $em->flush();
+
+        $isCurrentUser = $this->getUser() === $user;
+
+        if ($isCurrentUser) {
+            $tokenStorage->setToken(null);
+            $session->invalidate();
+        }
+
+        $em->remove($user);
+        $em->flush();
+
+        $this->addFlash('success', 'Utilisateur supprimé avec succès.');
+        return $this->redirectToRoute($isCurrentUser ? 'app_logout' : 'admin_users');
+    }
 }
