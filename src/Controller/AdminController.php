869aaaccbb3eea1e30342132f2ccc1d516141464
VioletteCrochet <violettecrc@gmail.com>
2025-02-26 14:28:55 +0100

    feat(admin, user): add userlist from admin vue

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,19 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/admin')]
+#[Route('/admin', name: 'admin_')]
 final class AdminController extends AbstractController
 {
-    #[Route('/dashboard', name: 'admin_dashboard', methods: ['GET'])]
+    #[Route('/dashboard', name: 'dashboard', methods: ['GET'])]
     #[IsGranted('ROLE_ADMIN')]
     public function dashboard(): Response
     {
         return $this->render('admin/dashboard.html.twig');
     }
+
+    #[Route('/users', name: 'users', methods: ['GET'])]
+    public function users(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render('admin/users.html.twig', [
+            'users' => $users,
+            ]);
+    }
+
 #
 }
