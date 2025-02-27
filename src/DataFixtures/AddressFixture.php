34701b5ed868bf6845dcf5d71ceaaf9c27f9241d
VioletteCrochet <violettecrc@gmail.com>
2025-02-27 13:34:06 +0100

    feat(fixture, address) : wip address fixture

--- src/DataFixtures/AddressFixture.php
+++ src/DataFixtures/AddressFixture.php
@@ -2,15 +2,28 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Address;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
-class AddressFixture extends Fixture
+class AddressFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR'); // Génération de données en français
+
+        for ($i = 0; $i < 20; $i++) {
+            $address = new Address();
+            $address->setName($faker->company) // Nom du lieu
+            ->setStreet($faker->streetAddress) // Rue et numéro
+            ->setLat($faker->latitude) // Latitude aléatoire
+            ->setLng($faker->longitude) // Longitude aléatoire
+            ->setCity($faker->city) // Ville
+            ->setZipCode($faker->postcode); // Code postal
+
+            $manager->persist($address);
+        }
 
         $manager->flush();
     }
