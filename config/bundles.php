d96965ece6b69103e4addcf12289fb817d211f60
VioletteCrochet <oliviercrochet1@gmail.com>
2025-03-01 10:37:39 +0100

    style(header): add symfony ux typed on header

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\Typed\TypedBundle::class => ['all' => true],
 ];
